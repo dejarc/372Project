a5b876f4f94d91295157c797e89500898da4b0f6
markinos <markinos@uw.edu>
2016-02-08 16:12:25 -0800

    Alu command update
    Should we do the alu with bit logic operators or actually bit by bit?
    Added four additional commands and descriptions to .h file

--- alu.h
+++ alu.h
@@ -26,9 +26,12 @@ void alu_Dr(alu_);
 void alu_LdA(alu_);
 void alu_LdB(alu_);
 
-void alu_addb(alu_);
-void alu_nand(alu_);
-void alu_asub(alu_);
-void alu_ainc(alu_);
-
+void alu_addb(alu_); //000	a + b
+void alu_nand(alu_); //001	a nand b
+void alu_asub(alu_); //010	a - b
+void alu_ainc(alu_); //011	a + 1
+void alu_aorb(alu_); //100	a or b
+void alu_andb(alu_); //101	a and b
+void alu_axrb(alu_); //110	a xor b
+void alu_nota(alu_); //111	not a
 #endif
