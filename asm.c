0a65f25adcb92c064f3a0f42245bdeb5e507a139
dejarc <dejarc@uw.edu>
2016-03-01 04:12:26 -0800

    header file added

--- asm.c
+++ asm.c
@@ -3,16 +3,13 @@
  * Course Project LC2200
  */
 
-#ifndef BIT_H
-#define BIT_H
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stdint.h>
-#include "bit.h"
-#define OPCODE_LEN 4
-#define WORD_LEN 32
+#include <stdint.h>*/
+//#define WORD_LEN 32
+#include "asm.h"
 char inst[WORD_LEN + 1];
 int arg_num;
 void instructionHelper(char *input) {
@@ -20,13 +17,14 @@ void instructionHelper(char *input) {
     int index;
     if(strcmp(input, "add") == 0) {
         opcode = 0;   
+    } else if(strcmp(input, "nand") == 0) {
+        opcode = 1;
     } else if(strcmp(input, "addi") == 0) {
         opcode = 2;
     } else if(strcmp(input, "sw") == 0) {
         opcode = 4;  
     } else if(strcmp(input, "lw") == 0) {
         opcode = 3; 
-    
     } else if(strcmp(input, "beq") == 0) {
         opcode = 5;
     } else if(strcmp(input, "j") == 0) {
@@ -75,15 +73,18 @@ void parseInput(char *input) {
     int index;
     int start_bit;
     int imm = (int)strtol(input, NULL, 10); 
+    int instruction_one = (input[0] == 'a') | (input[0] == 'b');
+    int instruction_two = (input[0] == 's') | (input[0] == 'l');
+    int instruction_three = (input[0] == 'n');
     if(!imm) {
-        if(input[0] == 'a' || input[0] == 'b' || input[0] == 's' || input[0] == 'l') {
+        if(instruction_one || instruction_two || instruction_three) {
         //printf("\n argument was detected");
             instructionHelper(input);
         } else if(input[0] == '$') {
             arg_num++;
             argumentHelper(input);
         }
-    } else {
+    } else if(arg_num > 0) {
         for(index = 0; index < WORD_LEN; index++) {
             if(1 & (imm >> index))
                 inst[index] = '1';
@@ -95,22 +96,31 @@ void initializeInst() {
     for(index; index < WORD_LEN; index++) {
         inst[index] = '0';
     }
-} 
-int main(FILE *file) {
+}
+
+char *getInstruction(char *line) {
+    char *tokenPtr = strtok(line, " ,;.\n");
+    arg_num = 0;
+    initializeInst();
+    while(tokenPtr != NULL) {
+        if(strcmp(tokenPtr,"ORIG") == 0) 
+            return "";
+        parseInput(tokenPtr);
+        tokenPtr = strtok(NULL, " ,;().\n");
+    }
+    return inst;
+}
+ 
+int main(int argc, char **argv) {
     FILE *input_file;
-    input_file = fopen("input.txt", "r");
+    if (argc > 1) {
+        input_file = fopen(argv[1], "r");
+    } else {
+        input_file = fopen("input.txt", "r");
+    }
     char line[256];
     while (fgets(line, sizeof(line), input_file)) {    
-        char *tokenPtr = strtok(line, " ,;\n");
-        arg_num = 0;
-        initializeInst();
-        while(tokenPtr != NULL) {
-            parseInput(tokenPtr);
-            //printf("\n");
-            //printf("%s", tokenPtr);
-            tokenPtr = strtok(NULL, " ,;()\n");
-        }
-        printf("\nthe value of index is %s", inst); 
+        printf("\nthe value of index is %s", getInstruction(line)); 
     } 
     return 0;
 }
@@ -120,5 +130,4 @@ int main(FILE *file) {
 
 
 
-#endif
 
---
+++ asm.h
@@ -0,0 +1,7 @@
+#include "bit.h"
+#define OPCODE_LEN 4
+void initializeInst(); 
+void argumentHelper(char *); 
+void parseInput(char *); 
+void instructionHelper(char *);
+char *getInstruction(char *); 
