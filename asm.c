1be37c1dd94b5e5630c7c0f6b332a02b029a381c
dejarc <dejarc@uw.edu>
2016-03-07 23:34:02 -0800

    added line numbers

--- asm.c
+++ asm.c
@@ -14,6 +14,9 @@ static int hex_input;
 static int first_scan;
 static int num_branches;
 static int max_branches;
+static int comment_line;
+static int num_rows;
+
 void instructionHelper(char *input, char *line) {
     int opcode = 0;
     int index;
@@ -64,33 +67,43 @@ void argumentHelper(char *input, char* line) {
     } else if (input[1] == 'v') {
         reg_offset = 2;
     } else if(input[1] == 'f') {
+        
         reg_offset = 14;
     }
     if(num_offset >= 0 && num_offset <= 2) {
         reg_offset += num_offset;
-        range = 0;
-        if(arg_num < 3) {
-            range = WORD_LEN - ((arg_num + 1) * (OPCODE_LEN));
-        } 
-        for(index = range; index < range + OPCODE_LEN; index++) {
-            if(1 & (reg_offset >> index - range))
-                line[index] = '1';
-        }     
-    }
+    }     
+   //printf("\nwriting to bits %d with arg type %s", reg_offset, input);
+    range = 0;
+    if(arg_num < 3) {
+        range = WORD_LEN - ((arg_num + 1) * (OPCODE_LEN));
+    } 
+    for(index = range; index < range + OPCODE_LEN; index++) {
+        if(1 & (reg_offset >> index - range))
+            line[index] = '1';
+    }     
 }
 void parseInput(char *input, char *line) {
     int index;
     int start_bit;
-    int imm = (int)strtol(input, NULL, 10); 
-    int instruction_one = (input[0] == 'a') | (input[0] == 'b') | (input[0] == 'j');
-    int instruction_two = (input[0] == 's') | (input[0] == 'l');
-    int instruction_three = (input[0] == 'n');
+    char temp[strlen(input) + 1];   
+    if(input[strlen(input) - 1] == ';' && !first_scan) {
+        temp[strlen(input) + 1];
+        strncpy(temp, input, strlen(input) - 1);
+    } else {
+       strncpy(temp, input, strlen(input)); 
+    }  
+    
+    int imm = (int)strtol(temp, NULL, 10); 
+    int instruction_one = (temp[0] == 'a') | (temp[0] == 'b') | (temp[0] == 'j');
+    int instruction_two = (temp[0] == 's') | (temp[0] == 'l');
+    int instruction_three = (temp[0] == 'n');
     if(!imm) {
-        if(instruction_one || instruction_two || instruction_three || strcmp(input,"halt") == 0) {
-            instructionHelper(input, line);
-        } else if(input[0] == '$') {
+        if(instruction_one || instruction_two || instruction_three || strcmp(temp,"halt") == 0) {
+            instructionHelper(temp, line);
+        } else if(temp[0] == '$') {
             arg_num++;
-            argumentHelper(input, line);
+            argumentHelper(temp, line);
         }
     } else if(arg_num > 0 || hex_input) {
         if(hex_input) {
@@ -102,9 +115,9 @@ void parseInput(char *input, char *line) {
         }   
     }
 }
-void initializeInst(char *line) {
+void initializeInst(char *line, int range) {
     int index = 0;
-    for(index; index < WORD_LEN; index++) {
+    for(index; index < range; index++) {
         line[index] = '0';
     }
     line[index] = '\0';
@@ -112,14 +125,16 @@ void initializeInst(char *line) {
 char **getAllInstructions(FILE *fp) {
     char **bin_array;
     char **br_labels;
+    char **line_numbers;
     int *br_lines;
     int index;
-    int num_rows = 0;
+    num_rows = 0;
     char *temp_word = malloc(WORD_LEN);
     int max_rows = MAX_ROWS;
     max_branches = MAX_ROWS;
     num_branches = 0;
     bin_array = malloc(MAX_ROWS * sizeof(WORD_LEN) * sizeof(char *)); 
+    line_numbers = malloc(MAX_ROWS * sizeof(WORD_LEN) * sizeof(char *));   
     br_labels = malloc(MAX_ROWS * sizeof(char *)); 
     br_lines = malloc(MAX_ROWS * sizeof(int));
     char line[256];
@@ -128,15 +143,16 @@ char **getAllInstructions(FILE *fp) {
         if(num_rows - 1 == max_rows) {
             max_rows *= 2;
             bin_array = (char **)realloc(bin_array, sizeof(char *) * max_rows * sizeof(WORD_LEN));
+            line_numbers = (char **)realloc(line_numbers, sizeof(char *) * max_rows * sizeof(WORD_LEN));
         }
-        bin_array[num_rows] = getInstruction(line, br_labels, br_lines); 
+        bin_array[num_rows] = getInstruction(line, line_numbers, br_labels, br_lines); 
         num_rows++; 
     }
     num_rows = 0;
     fseek(fp, 0, SEEK_SET); 
     first_scan = false;
     while (fgets(line, sizeof(line), fp)) {    
-        bin_array[num_rows] = getInstruction(line, br_labels, br_lines);
+        bin_array[num_rows] = getInstruction(line, line_numbers, br_labels, br_lines);
         strcpy(temp_word, bin_array[num_rows]);
         if(strcmp(bin_array[num_rows],"COMMENT") != 0) {
             if(strcmp(bin_array[num_rows], "END") == 0) {
@@ -148,11 +164,45 @@ char **getAllInstructions(FILE *fp) {
             for(index = 0; index < WORD_LEN; index++) {
                 bin_array[num_rows][index] = temp_word[(WORD_LEN - 1) - index];
             }
+            strcpy(temp_word, line_numbers[num_rows]);
+            for(index = 0; index < WORD_LEN; index++) {
+                line_numbers[num_rows][index] = temp_word[(WORD_LEN - 1) - index];
+            }
+            //printf("\nvalue of line numbers %s", line_numbers[num_rows]);
+            //printf("\nvalue of bin    array %s", bin_array[num_rows]);
+            //initializeInst(everything[num_rows], WORD_LEN * 2);
+            /*for(index = 0; index < WORD_LEN; index++) {
+                everything[num_rows][index] = line_numbers[num_rows][index];
+            }
+            for(index = WORD_LEN; index < WORD_LEN * 2; index++) {
+                everything[num_rows][index] = bin_array[num_rows][index - WORD_LEN];
+            }*/
             num_rows++;
-        }
+        } 
     }
+    int column;
+    char **everything = malloc(sizeof(char *) * WORD_LEN * 2 * num_rows);
+    for(index = 0; index < num_rows; index++) { 
+        everything[index] = malloc(WORD_LEN * 2);
+        for(column = 0; column < WORD_LEN * 2; column++) {
+            everything[index][column] = '0';
+        }
+        everything[index][column] = '\0';
+    }
+    index = 0;
+    while(bin_array[index] != NULL & line_numbers[index] != NULL) {
+        for(column = 0; column < WORD_LEN; column++) {
+            everything[index][column] = '0';
+            everything[index][column] = line_numbers[index][column];
+        }
+        for(column = WORD_LEN; column < WORD_LEN * 2; column++) {
+            everything[index][column] = bin_array[index][column - WORD_LEN];
+        }
+        index++;
+    }
+    
     free(temp_word);
-    return bin_array;
+    return everything;
 }
 int hexConvert(int num) {
     int converted_num = 0;
@@ -164,18 +214,21 @@ int hexConvert(int num) {
     }
     return converted_num;
 }
-char *getInstruction(char *line, char **br_labels, int *br_lines) {
-    char *tokenPtr = strtok(line, " ,:\n");
+char *getInstruction(char *line, char **line_numbers, char **br_labels, int *br_lines) {
+    char *tokenPtr = strtok(line, " ,:()\t\n");
+    char temp_line[32]; 
     arg_num = 0;
     hex_input = false;
     int token_ctr = 0; 
     int branch_index;
     char *new_line = (char *)malloc(WORD_LEN + 1);
-    initializeInst(new_line);
+    initializeInst(new_line, WORD_LEN);
     int branch_found = false;
     int line_num = 0;
     int index = 0;
     int prev_label = false;
+    int line_set = false;
+    comment_line = false;
     while(tokenPtr != NULL) {
         //printf("\nthe token is %s", tokenPtr);
         /*if(tokenPtr[strlen(tokenPtr) - 1] == ';' || tokenPtr[0] == ';') {
@@ -208,6 +261,32 @@ char *getInstruction(char *line, char **br_labels, int *br_lines) {
             } else if(num_branches > 0 && br_lines[num_branches - 1] == 0 && first_scan) {// branch value not set, set value here
                 br_lines[num_branches - 1] = line_num + 1;
             }
+            if(!first_scan && !line_set){
+                initializeInst(temp_line, WORD_LEN);
+                for(index = 0; index < WORD_LEN; index++) {
+                    if(1 & (line_num >> index)) {
+                        temp_line[index] = '1';
+                    } else {
+                        temp_line[index] = '0';
+                    } 
+                }
+                line_numbers[num_rows] = malloc(sizeof(temp_line));
+                strcpy(line_numbers[num_rows], temp_line);
+                line_set = true;
+            } 
+        }
+        if(token_ctr >= 1 && line_num == 0 && !first_scan && !line_set) {
+            int temp_num = num_rows;
+            initializeInst(temp_line, WORD_LEN);
+            for(index = 0; index < WORD_LEN; index++) {
+                if(1 & (temp_num >> index)) {
+                    temp_line[index] = '1';
+                } else {
+                    temp_line[index] = '0';
+                }
+            } 
+            line_numbers[num_rows] = malloc(sizeof(temp_line));
+            strcpy(line_numbers[num_rows], temp_line);
         }
         if(label_dest && branch_found && first_scan ) {//this is to solve the instance of a label showing up after a line number, in a previously stored branch
             br_lines[branch_index] -= line_num;
@@ -235,13 +314,13 @@ char *getInstruction(char *line, char **br_labels, int *br_lines) {
             }   
         } 
         parseInput(tokenPtr, new_line);
-        if(tokenPtr[strlen(tokenPtr) - 1] == ';' || tokenPtr[0] == ';') {
+        if((tokenPtr[strlen(tokenPtr) - 1] == ';' || tokenPtr[0] == ';') && !first_scan) {
             if(token_ctr == 1) {
-                return "COMMENT";
+                return "COMMENT"; 
             }
             return new_line;
         }
-        tokenPtr = strtok(NULL, " ,:()\n");
+        tokenPtr = strtok(NULL, " ,:()\t\n");
     }
     if(first_scan) {//initial scan, return empty string 
         free(new_line);
@@ -260,11 +339,12 @@ int main(int argc, char **argv) {
         input_file = fopen(argv[1], "r");
     } else {
         input_file = fopen("input.txt", "r");
+        if(input_file == NULL)
+            printf("\ncannot open this file!" ); 
     }
-    //printf("\nsize of array is %d ", (int)strlen(bin_array));
+    printf("\nsize of array is " );
     bin_array = getAllInstructions(input_file);
     while(bin_array[num_rows] != NULL) {
-        printf("\nassembly instruction at row %d is %s", num_rows, bin_array[num_rows]);
         num_rows++;
     }
     return 0;
--- asm.h
+++ asm.h
@@ -1,10 +1,9 @@
 #include "bit.h"
 #define OPCODE_LEN 4
-void initializeInst(char *); 
+void initializeInst(char *, int); 
 void argumentHelper(char *, char *); 
 void parseInput(char *, char *); 
 void instructionHelper(char *, char *);
 char **getAllInstructions(FILE *); 
 int hexConvert(int ); 
-char *getInstruction(char *, char**, int*); 
-int debugCommandLine(int, char **);
+char *getInstruction(char *, char **, char**, int*); 
